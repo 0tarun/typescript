fd5c4eab690be11f067ed59a706533e8f398875c
Tarun <tdas221498@bscse.uiu.ac.bd>
2026-07-30 18:10:47 +0600

    array

---
+++ array.js
@@ -0,0 +1,6 @@
+const team = [];
+team.push("Rudra Kumar Das");
+team.push("Tarun Chandra Das");
+team.push("Abir Chandra Das");
+const number = [7, 10, 9];
+export {};
---
+++ array.ts
@@ -0,0 +1,7 @@
+const team: string[] = [];
+team.push("Rudra Kumar Das");
+team.push("Tarun Chandra Das");
+team.push("Abir Chandra Das");
+
+
+const number: number[] = [7,10,9];
